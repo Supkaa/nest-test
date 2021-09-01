9e8815903553f364f292d778def79dd5e1eba334
Maxim Khludeev <m.a.khludeev@esoft.local>
2021-09-01 15:58:49 +0500

    test

---
+++ src/config/database/postgres/database-postgres-config.module.ts
@@ -0,0 +1,24 @@
+import * as Joi from '@hapi/joi';
+import { Module } from '@nestjs/common';
+import { AppConfigService } from './app-config.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './configuration';
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			load: [configuration],
+			validationSchema: Joi.object({
+				APP_NAME: Joi.string().default('Test'),
+				APP_ENV: Joi.string()
+					.valid('development', 'production', 'test')
+					.default('development'),
+				APP_URL: Joi.string().default('http://nest-first-project'),
+				APP_PORT: Joi.number().default(3000),
+			}),
+		}),
+	],
+	providers: [AppConfigService, ConfigService],
+	exports: [AppConfigService, ConfigService],
+})
+export class AppConfigModule {}
